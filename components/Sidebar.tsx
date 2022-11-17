6799fbe63312cff3a77834fbe7c1803f89d706e0
unonao <naokiuno634@gmail.com>
2022-11-17 18:18:59 +0900

    レイアウト修正

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,28 @@
+/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
+/** @jsxImportSource @emotion/react */
 import * as React from "react";
 import Script from "next/script";
+import { css } from "@emotion/react";
+
+const twitterHeight = 800;
 
 function EmbeddedTwitter() {
   return (
-    <>
+    <div
+      css={css`
+        height: ${twitterHeight}px;
+      `}
+    >
       <a
         className="twitter-timeline"
         data-theme="light"
-        data-height="800"
+        data-height={twitterHeight}
         href="https://twitter.com/LAIME_AI?ref_src=twsrc%5Etfw"
       >
         Tweets by LAIME_AI
       </a>{" "}
       <Script async src="https://platform.twitter.com/widgets.js" />
-    </>
+    </div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import type { NextPage } from "next";
 import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+
+const SectionBox = styled(Box)({
+  padding: "20px 0px",
+});
 
 type Props = {
   issues: Array<Issue>;
@@ -10,12 +16,46 @@ type Issue = any;
 const Home: NextPage<Props> = () => {
   return (
     <section>
-      <Typography>
-        LAIMEは国内の情報系学生&医学生を中心とした機械学習の勉強コミュニティです。
-        機械学習に興味がある情報系学生あるいはMedtechに興味がある医学生などが全国から集まり、主にslack,
-        zoom, google meet,
-        gatherなどを使用して共に勉強会、コンペなどを開催しています。
-      </Typography>
+      <SectionBox>
+        <Typography variant="h2" component="h1">
+          LAIME とは
+        </Typography>
+        <Typography>
+          LAIME (Leaders of AI & MEdicine)
+          は国内の情報系学生&医学生を中心とした機械学習の勉強コミュニティです。
+        </Typography>
+        <Typography>
+          機械学習に興味がある情報系学生あるいはMedtechに興味がある医学生などが全国から集まり、主にSlack,
+          zoom, google meet,
+          gatherなどのツールを使用してオンラインで共に勉強会、inclass
+          コンペなどを開催しています。
+        </Typography>
+      </SectionBox>
+
+      <SectionBox>
+        <Typography variant="h3" component="h2">
+          団体理念
+        </Typography>
+
+        <ul>
+          <li>各個人のプログラミング能力を向上させること</li>
+          <li>メンバー間でのとの交流を深めること</li>
+        </ul>
+        <Typography>
+          各個人のプログラミング能力を向上させるというのは、具体的にはkaggleを通してPytorchやTensor
+          Flow等の使い方をはじめ、機械学習周辺のライブラリを使いこなせるようになることを目指します。
+        </Typography>
+        <Typography>
+          LAIMEでの勉強会企画、招待制コンペの開催などに関してはこの理念を念頭に置いています。
+        </Typography>
+
+        <Typography>
+          次に、LAIME内ののメンバーとの交流を深めてもらうことが挙げられます。
+        </Typography>
+        <Typography>
+          PCとインターネット環境があれば物理的距離の問題こそ解消されますが、開発者同士の心理的距離が埋まらなければアイデアは生まれず開発は始まりません。LAIMEでは一度知り合った人とは連絡をとり続け、いつか誰かが新しいことを始めるときに他の誰かにすぐサポートを頼めるような人的ネットワークを作ることを目指します。
+        </Typography>
+      </SectionBox>
     </section>
   );
 };
