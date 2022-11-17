2cbb33dade7843754476b868c5d5209c5333623a
unonao <naokiuno634@gmail.com>
2022-11-17 20:48:21 +0900

    faviconを修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href={makeImagePath(`favicon.png`)} />
+          <link rel="shortcut icon" href={makeImagePath(`favicon.ico`)} />
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
         </Head>
